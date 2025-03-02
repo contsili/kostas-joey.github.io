761d64551f2bc8650c9ea01c28de2bdf2a83032f
Konstantinos Tsilimparis <konstantinos@artinis.com>
2025-03-02 21:51:28 +0100

    firebase

--- js/elo.js
+++ js/elo.js
@@ -1,3 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs } from "firebase/firestore"; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD1nnM8PbImTnRPdr6O2Nkcsm_6k22XHBo",
+    authDomain: "foosball-elo-53e01.firebaseapp.com",
+    projectId: "foosball-elo-53e01",
+    storageBucket: "foosball-elo-53e01.firebasestorage.app",
+    messagingSenderId: "520975826180",
+    appId: "1:520975826180:web:58daa9f0fc6327a0036451"
+  };
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // Initialize player ratings dictionary
 let playerRatings = {};
 
